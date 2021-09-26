30e4aab5b528f2d15e2898e05da81c02170fac6a
clementGit23 <clementchoeur59@gmail.com>
2021-09-26 12:50:51 +0200

    Question 3 (minimum)

--- Choeur Clement/TD1-Exercice4/main.c
+++ Choeur Clement/TD1-Exercice4/main.c
@@ -1,64 +1,84 @@
-#include <stdio.h>
-# define N 10
+    #include <stdio.h>
+    # define N 10
 
-void samePosition(int tab1[], int tab2[]){
+    void samePosition(int tab1[], int tab2[]){
 
-	for(int i=0; i < N; i++){
-        printf("Enter value for position %d : ",i);
-        scanf("%d\n", &tab1[i]);
-	}
-
-	for(int j=0; j < N; j++){
-        if(tab1[j] < 0){
-            tab2[j] = 0;
+        for(int i=0; i < N; i++){
+            printf("Enter value for position %d : ",i);
+            scanf("%d\n", &tab1[i]);
         }
-        else
-        {
-            tab2[j] = tab1[j];
+
+        for(int j=0; j < N; j++){
+            if(tab1[j] < 0){
+                tab2[j] = 0;
+            }
+            else
+            {
+                tab2[j] = tab1[j];
+            }
         }
-	}
 
-}
+    }
 
-void average(int tab[]){
+    void average(int tab[]){
 
-    int sum = 0;
-    int avg = 0;
+        int sum = 0;
+        int avg = 0;
 
-    for(int i=0; i < N; i++){
-        printf("Enter value for position %d :\n", i);
-        scanf("%d", &tab[i]);
-	}
+        for(int i=0; i < N; i++){
+            printf("Enter value for position %d :\n", i);
+            scanf("%d", &tab[i]);
+        }
 
-	for(int j=0; j < N; j++){
-        sum = sum + tab[j];
-	}
+        for(int j=0; j < N; j++){
+            sum = sum + tab[j];
+        }
 
-	avg = sum / N;
+        avg = sum / N;
 
-	return avg;
-}
+        return avg;
+    }
 
-void afficherTableau(int tab[]){
-	for(int i=0; i< N; i++){
-		printf("at position %d, value %d\n",i, tab[i]);
-		}
-    printf("\n");
-	}
+    void min(int tab[]){
 
-int main(int argc, char **argv)
-{
-	printf("TD1- Tableau \n");
+        int min;
+        min = tab[0];
 
-	int mytab1[N];
-	// int mytab2[N];
+        for(int i=0; i < N; i++){
+            printf("Enter value for position %d :\n", i);
+            scanf("%d", &tab[i]);
+        }
 
-	// samePosition(mytab1, mytab2);
+        for(int j=0; j < N; j++){
+            if(min > tab[j])
+                min = tab[j];
+        }
 
-	// afficherTableau(mytab1);
-	// afficherTableau(mytab2);
+        printf("Minimum value is : %d\n", min);
+    }
 
-	average(mytab1);
+    void afficherTableau(int tab[]){
+        for(int i=0; i< N; i++){
+            printf("at position %d, value %d\n",i, tab[i]);
+            }
+        printf("\n");
+        }
 
-	return 0;
-}
+    int main(int argc, char **argv)
+    {
+        printf("TD1- Tableau \n");
+
+        int mytab1[N];
+        // int mytab2[N];
+
+        // samePosition(mytab1, mytab2);
+
+        // afficherTableau(mytab1);
+        // afficherTableau(mytab2);
+
+        // average(mytab1);
+
+        min(mytab1);
+
+        return 0;
+    }
