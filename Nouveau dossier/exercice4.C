53d60d3686e0c012075f04b1f96d3e82eaba64fe
Arthur57330 <arthur.trus1@gmail.com>
2021-09-27 17:29:57 +0200

    Update exercice4.C

--- Nouveau dossier/exercice4.C
+++ Nouveau dossier/exercice4.C
@@ -30,14 +30,30 @@ for (int i = 0; i < 10; i++)
 }
 }
 
+int moyenne(Tab tab)// return la moyenne du tableau 
+{
+int somme=0,moyenne=0,nombre=0;
+
+for (int i = 0; i < 10; i++)
+{
+    somme= somme + tab[i];
+    nombre++;
+}
+
+ moyenne = somme/ nombre;
+return moyenne;
+
+}
+
 int main(void)
 {
 Tab tab1[]= {45,3,-33,54,574,1,71,18};
 Tab tab2[]= {0};
-
+int moyennes;
 
 recopie(tab1,tab2);
-affichage(tab1);
 mettreAZero(tab2);
-
+moyennes = moyenne(tab1);
+printf("moyenne est: %d\n", moyennes);
 return 0;
+}
