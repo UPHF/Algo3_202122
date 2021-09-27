f87a4e36cafb98bfbf783b42491d6459af90fe2c
XProKio <90325305+XProKio@users.noreply.github.com>
2021-09-27 18:38:30 +0200

    Exo 4 part 3

--- Ethan D'alessandro/main.c
+++ Ethan D'alessandro/main.c
@@ -1,66 +1,85 @@
 #include <stdio.h>
 # define N 10
-void exactPosition(int tab1[], int tab2[]){
-	for(int i=0; i < N; i++){
-        printf("Entrer une valeur pour la position %d : ",i);
-        scanf("%d\n", &tab1[i]);
-	}
-	for(int j=0; j < N; j++){
-        if(tab1[j] < 0){
-            tab2[j] = 0;
+
+    void exactPosition(int tab1[], int tab2[]){
+
+        for(int i=0; i < N; i++){
+            printf("Entrer la valeur %d : ",i);
+            scanf("%d\n", &tab1[i]);
         }
-        else
-        {
-            tab2[j] = tab1[j];
+
+        for(int j=0; j < N; j++){
+            if(tab1[j] < 0){
+                tab2[j] = 0;
+            }
+            else
+            {
+                tab2[j] = tab1[j];
+            }
         }
-	}
 
-}
+    }
 
-void moyenne(int tab[]){
+    void moyenne(int tab[]){
 
-    int total = 0;
-    int moyenne = 0;
+        int total = 0;
+        int moyenne = 0;
 
-    for(int i=0; i < N; i++){
-        printf("Entrer une valeur pour la position %d :\n", i);
-        scanf("%d", &tab[i]);
-	}
+        for(int i=0; i < N; i++){
+            printf("Entrer la valeur %d :\n", i);
+            scanf("%d", &tab[i]);
+        }
 
-	for(int j=0; j < N; j++){
-        total = total + tab[j];
-	}
+        for(int j=0; j < N; j++){
+            total = total + tab[j];
+        }
 
-	moyenne = total / N;
+        moyenne = total / N;
 
-	return moyenne;
-}
+        return moyenne;
+    }
 
-void showTable(int tab[]){
-	for(int i=0; i< N; i++){
-		printf("à la position %d, valeur %d\n",i, tab[i]);
-		}
-	printf("\n");
+    void minimum(int tab[]){
 
-	}
-	
-	int main(int argc, char **argv){
-	printf("TD1- Tableau \n");
+        int minimum;
+        minimum = tab[0];
 
-	int monTab1[N];
-	int monTab2[N];
-	// int monTab[N];
+        for(int i=0; i < N; i++){
+            printf("Entrer la valeur %d :\n", i);
+            scanf("%d", &tab[i]);
+        }
 
-	// samePosition(monTab1, monTab2);
+        for(int j=0; j < N; j++){
+            if(minimum > tab[j])
+                minimum = tab[j];
+        }
 
-	exactPosition(monTab1, monTab2);
-	// afficherTableau(monTab1);
-	// afficherTableau(monTab2);
+        printf("la valeur minimal est : %d\n", minimum);
+    }
 
-	showTable(monTab1);
-	showTable(monTab2);
-	moyenne(monTab1);
 
-	return 0;
-	
-}
+    void showTable(int tab[]){
+        for(int i=0; i< N; i++){
+            printf("à la position %d, valeur %d\n",i, tab[i]);
+            }
+        printf("\n");
+        }
+
+    int main(int argc, char **argv)
+    {
+        printf("TD1- Tableau \n");
+
+        int monTab1[N];
+        // int montab2[N];
+
+        // exactPosition(monTab1, monTab2);
+
+        // showTable(monTab1);
+        // showTable(monTab2);
+
+        // moyenne(monTab1);
+
+        minimum(monTab1);
+
+        return 0;
+    } 
