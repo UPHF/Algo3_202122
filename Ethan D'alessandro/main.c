4a41c7ec56d72f79d0836ff3b495859346790063
XProKio <90325305+XProKio@users.noreply.github.com>
2021-09-27 18:25:25 +0200

    Exo 4 part2

--- Ethan D'alessandro/main.c
+++ Ethan D'alessandro/main.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-#define N 10
-
+# define N 10
 void exactPosition(int tab1[], int tab2[]){
-
 	for(int i=0; i < N; i++){
-        printf("Entrer la valeur pour la position %d : ",i);
+        printf("Entrer une valeur pour la position %d : ",i);
         scanf("%d\n", &tab1[i]);
 	}
-
 	for(int j=0; j < N; j++){
         if(tab1[j] < 0){
             tab2[j] = 0;
@@ -20,24 +17,50 @@ void exactPosition(int tab1[], int tab2[]){
 
 }
 
+void moyenne(int tab[]){
+
+    int total = 0;
+    int moyenne = 0;
+
+    for(int i=0; i < N; i++){
+        printf("Entrer une valeur pour la position %d :\n", i);
+        scanf("%d", &tab[i]);
+	}
+
+	for(int j=0; j < N; j++){
+        total = total + tab[j];
+	}
+
+	moyenne = total / N;
+
+	return moyenne;
+}
+
 void showTable(int tab[]){
 	for(int i=0; i< N; i++){
 		printf("à la position %d, valeur %d\n",i, tab[i]);
 		}
-    printf("\n");
-	}
+	printf("\n");
 
-int main(int argc, char **argv)
-{
+	}
+	
+	int main(int argc, char **argv){
 	printf("TD1- Tableau \n");
 
 	int monTab1[N];
 	int monTab2[N];
+	// int monTab[N];
+
+	// samePosition(monTab1, monTab2);
 
 	exactPosition(monTab1, monTab2);
+	// afficherTableau(monTab1);
+	// afficherTableau(monTab2);
 
 	showTable(monTab1);
 	showTable(monTab2);
+	moyenne(monTab1);
 
 	return 0;
-} 
+	
+}
