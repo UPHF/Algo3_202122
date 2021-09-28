dbb0a408cfec90919277502f8431e94f8397fc71
AdamHsn <90774443+AdamHsn@users.noreply.github.com>
2021-09-28 12:29:18 +0200

    Hansen Adam Exo4

---
+++ Exo 4 n4.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+# define N 10
+// Exercise 4 ---> 4
+
+// EXERCISE 1 :
+void InputTable(int tab1[], int tab2[]){
+	for(int i=0; i < N; i++){
+            printf("Write the %d value :\n",i); //Entry values
+    scanf("%d", &tab1[i]);
+            tab2[i] = 0; // Fix value tab2 to 0
+			if (tab1[i] > 0){ // "if" to put 0 for negative values
+                tab2[i] = tab1[i]; // Transfer positive values tab1 to tab2
+			}
+	}
+}
+
+void ShowTable(int tab1[], int tab2[]){ //function showing table
+	for(int i=0; i< N; i++){
+		printf("Table 2 : at position %d, value %d\n",i, tab2[i]); // Print tab2's values
+		}
+	}
+
+// EXERCISE 2 :
+void AvgTable (int tab[]){
+    int avg;
+    int sum;
+    int i;
+    for (i=0 ; i< N ; i++) //find the number of values in tab
+      {
+          sum += tab[i]; // sum of all numbers in tab
+      }
+avg = sum/N; // average calcul
+printf("The average of this table is %d\n", avg); // print  average
+}
+
+void InputTable2(int tab[]){
+	for(int i=0; i < N; i++){
+            printf("Write the %d value :\n",i); //Entry values
+    scanf("%d", &tab[i]);
+	}
+}
+
+// EXERCISE 3 :
+void MinTable(int tab[]){
+    int i;
+    int min = 0;
+for (i=0; i<N; i++)
+     {
+      if(tab[i]<tab[min])min=i; // Minimal value finding
+     }
+     printf("Minimal value is %d\n", tab[min]); // Showing minimal value
+}
+// EXERCISE 4 :
+void MinMaxTable(int tab[]){
+    int i;
+    int min = 0;
+    int max = 0;
+for (i=0; i<N; i++)
+     {
+      if(tab[i]<tab[min])min=i; // Minimal value finding
+      if(tab[i]>tab[max])max=i; // Maximal value finding
+     }
+     printf("Minimal value is %d\n", tab[min]); // print minimal value
+     printf("Maximal value is %d\n", tab[max]); // print maximal value
+}
+
+int main(int argc, char **argv) //main
+{
+    int tab1[N],  tab2[N], tab[N];
+
+//	InputTable(tab1, tab2);
+
+//	ShowTable(tab1, tab2);
+
+    InputTable2(tab);
+
+//	AvgTable (tab);
+
+//    MinTable(tab);
+
+    MinMaxTable(tab);
+
+	return 0;
+}
