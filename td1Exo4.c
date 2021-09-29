433b2cb4be55af09d4e253a5e337bc066cd91d5d
Maxime LEFEBVRE <lefebvremaxime825@gmail.com>
2021-09-29 15:17:48 +0200

    td1 Maxime premier commit

---
+++ td1Exo4.c
@@ -0,0 +1,33 @@
+/*
+    Author : Maxime LEFEBVRE
+    Year : 2021/2022
+    Subject : Test GitHub commits with exo 4 TD1
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int Tab[10];
+
+int copy(int t1, int t2)
+{
+    Tab tab1;
+    Tab tab2;
+
+    for (int i = 0; i < 10; i++){
+        if(tab1[i]>0){
+            tab2[i] = tab1[i];
+        }
+    }
+
+    //Remplissage t2 de 0 s'il reste des places
+
+    for(int i=0; i<t2; i++){
+        
+    }
+}
+
+int main(void)
+{
+    return 0;
+}
