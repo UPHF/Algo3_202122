64c112303c47f5041612a93b82499810c8e20795
martinezmatias <matias.sebastian.martinez@gmail.com>
2021-09-29 14:44:10 +0200

    declaration of struct Employer

---
+++ TD2-Exo1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+# define N 15 
+typedef struct{
+	char non[N];
+	char prenon[N];
+	int codeFonction;
+	float salaire;
+}Employe;
+
+int main(int argc, char **argv)
+{
+	printf("TD2\n");
+	return 0;
+}
