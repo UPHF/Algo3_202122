7e9104964d2563fee057e0e7556d8b0e7db894f6
Maxime LEFEBVRE <lefebvremaxime825@gmail.com>
2021-10-04 17:51:26 +0200

    Initialization Employe and TabEmploye

--- td2Exo3.c
+++ td2Exo3.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef char Chaine[20];
 
+typedef struct _Employe{
+    Chaine Prenom;
+    Chaine nom;
+    double salaire;
+}Employe;
+
+typedef Employe TabEmploye[100];
 
 int main(void)
 {
