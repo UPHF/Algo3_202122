fb1dbd598f8be42b9fb85310a5deb95af6f565ea
Maxime LEFEBVRE <lefebvremaxime825@gmail.com>
2021-10-04 18:21:36 +0200

    employe adding into a tabEmploye function + try with main function

--- td2Exo3.c
+++ td2Exo3.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Question 1
+
 typedef char Chaine[20];
 
 typedef struct _Employe{
@@ -11,7 +13,22 @@ typedef struct _Employe{
 
 typedef Employe TabEmploye[100];
 
+// Question 2
+
+void employeAdding(TabEmploye tabEmp, int nombreEmp){
+    for (int i = 0; i<nombreEmp; i++){
+        printf("\nPrenom de l'employe numero <%d> : ", i+1);
+        scanf("%s", &tabEmp->Prenom);
+        printf("Nom de l'employe numero <%d> : ", i+1);
+        scanf("%s", &tabEmp->nom);
+        printf("Salaire de l'employe numero <%d> : ", i+1);
+        scanf("%f", &tabEmp->salaire);
+    }
+}
+
 int main(void)
 {
+    TabEmploye tabEmp1;
+    employeAdding(tabEmp1, 2);
     return 0;
 }
