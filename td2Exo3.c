0209186f7ae3d1c4d4a4ac4ee109ffd4fe84290f
Maxime LEFEBVRE <lefebvremaxime825@gmail.com>
2021-10-06 10:48:45 +0200

    disp tab function BUT i can't make it fonctionnal cuase of pointers

--- td2Exo3.c
+++ td2Exo3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Question 1
 
@@ -8,27 +9,39 @@ typedef char Chaine[20];
 typedef struct _Employe{
     Chaine Prenom;
     Chaine nom;
-    double salaire;
-}Employe;
+    double *salaire;
+}Employe, *pEmploye;
 
-typedef Employe TabEmploye[100];
+typedef pEmploye *TabEmploye[100];
 
 // Question 2
 
-void employeAdding(TabEmploye tabEmp, int nombreEmp){
+void employeAdding(TabEmploye *tabEmp, int nombreEmp){
     for (int i = 0; i<nombreEmp; i++){
         printf("\nPrenom de l'employe numero <%d> : ", i+1);
-        scanf("%s", &tabEmp->Prenom);
+        scanf("%s", *(tabEmp)->prenom);
         printf("Nom de l'employe numero <%d> : ", i+1);
-        scanf("%s", &tabEmp->nom);
+        scanf("%s", *(tabEmp)->nom);
         printf("Salaire de l'employe numero <%d> : ", i+1);
-        scanf("%f", &tabEmp->salaire);
+        scanf("%f", *(tabEmp)->salaire);
+    }
+}
+
+void dispTabEmp(TabEmploye tabEmp, int nombreEmp){
+    for (int i=0; i<nombreEmp; i++){
+        printf("\nEmploye numero : %d\n", i+1);
+        printf("Prenom : %s\n", tabEmp->Prenom);
+        printf("Nom : %s\n", tabEmp->nom);
+        printf("Salaire : %f\n", tabEmp->salaire);
     }
 }
 
 int main(void)
 {
     TabEmploye tabEmp1;
-    employeAdding(tabEmp1, 2);
+    TabEmploye *pTabEmp1 = &tabEmp1;
+    
+    employeAdding(pTabEmp1, 2);
+    dispTabEmp(tabEmp1, 2);
     return 0;
 }
