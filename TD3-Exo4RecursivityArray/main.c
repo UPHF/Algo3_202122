3355ab8fb079266b02cd898a6096a83e9117ddd1
martinezmatias <matias.sebastian.martinez@gmail.com>
2021-10-06 14:03:22 +0200

    exo 4 Td 3 point a bis

--- TD3-Exo4RecursivityArray/main.c
+++ TD3-Exo4RecursivityArray/main.c
@@ -2,10 +2,6 @@
 
 //saisie récursive des N entiers de T (remplir le tableau complètement)
 
-void saisieRecursive(int tab[], int N){
-
-}
-
 void saisieIterative(int tab[], int N){
 	
 	for (int i=0; i< N; i++){
@@ -22,13 +18,38 @@ void afficherIterative(int tab[], int N){
 	}
 }
 
+void afficherRecursive(int tab[], int i){
+	if (i <= 0){
+			return ;
+	}
+	else{
+		afficherRecursive(tab, i -1);
+		printf("Recursive position %d: %d\n", i -1, tab[i-1]);
+	}
+}
+
+void saisieRecursive(int tab[], int i){
+	if (i <= 0){
+			return ;
+	}
+	else{
+		saisieRecursive(tab, i -1);
+		printf("Enter value for position %d\n", i -1);
+		scanf("%d", &tab[i-1]);
+	}
+}
 int main(int argc, char **argv)
 {
 	printf("TD3\n");
 	int N=5;
 	int tab[N];
-	saisieIterative(tab, N);
-	printf("Now we print the Array:\n");
-	afficherIterative(tab, N);
+	//saisieIterative(tab, N);
+	//printf("Now we print the Array Iterative:\n");
+	//afficherIterative(tab, N);
+	printf("Enter values for the array recursive: \n");
+	saisieRecursive(tab, N);
+	printf("Print the array recursive: \n");
+	afficherRecursive(tab, N);
+	
 	return 0;
 }
