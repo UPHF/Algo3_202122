69fdd56c56937e28c4df2009cf184a2881542c97
martinezmatias <matias.sebastian.martinez@gmail.com>
2021-10-06 14:55:08 +0200

    exo 4 Td 3 point b) debugging mode

--- TD3-Exo4RecursivityArray/main.c
+++ TD3-Exo4RecursivityArray/main.c
@@ -46,6 +46,50 @@ void afficherRecursiveDebug(int tab[], int i){
 	}
 }
 
+void afficherRecursiveDebugSpaces(int tab[], int i){
+	printf("[Debug]Current value of i: %d\n", i);
+	if (i < 0){
+			printf("[Debug]Inside Stop condition: (negative position does not exist)\n");
+			return ;
+	}
+	else{
+		printf("[Debug]Before calling afficherRecursiveDebug(tab, %d)\n", i-1);
+		afficherRecursiveDebug(tab, i -1);
+		printf("Recursive position %d: %d\n", i , tab[i]);
+	}
+}
+
+//calcul de la somme des N premiers éléments de T
+int sommeRecursiveDebug(int tab[], int i){
+	printf("[Debug]Current value of i: %d\n", i);
+	
+	if( i<0){
+		printf("[Debug]Inside Stop condition: (negative position does not exist)\n");
+		return 0;
+	}
+	else{
+		printf("[Debug] calling sommeRecursive(tab, %d)\n", i-1);
+	
+		int sumAtLeft =  sommeRecursiveDebug(tab, i-1);
+		printf("[Debug] results of sommeRecursive(tab, %d):%d\n", i-1, sumAtLeft);
+	
+		return  tab[i] + sumAtLeft;
+	}
+	
+}
+
+int sommeRecursive(int tab[], int i){
+	
+	if( i<0){
+		return 0;
+	}
+	else{
+		
+		return  tab[i] + sommeRecursive(tab, i-1);
+	}
+	
+}
+
 void saisieRecursive(int tab[], int i){
 	if (i < 0){
 			return ;
@@ -56,6 +100,18 @@ void saisieRecursive(int tab[], int i){
 		scanf("%d", &tab[i]);
 	}
 }
+
+void saisieRecursiveDebug(int tab[], int i){
+	if (i < 0){
+			return ;
+	}
+	else{
+		saisieRecursive(tab, i -1);
+		printf("Enter value for position %d\n", i );
+		scanf("%d", &tab[i]);
+	}
+}
+
 int main(int argc, char **argv)
 {
 	printf("TD3\n");
@@ -71,7 +127,8 @@ int main(int argc, char **argv)
 	saisieRecursive(tab, indexLastPosition);
 	printf("Print the array recursive: \n");
 	//afficherRecursive(tab, indexLastPosition);
-	afficherRecursiveDebug(tab, indexLastPosition);
-	
+	//afficherRecursiveDebug(tab, indexLastPosition);
+	int resultSum = sommeRecursiveDebug(tab, indexLastPosition);
+	printf("Result Sum value %d: \n", resultSum);
 	return 0;
 }
