c0109e48d11d5c7106d9781c83a31b511c5691b3
martinezmatias <matias.sebastian.martinez@gmail.com>
2021-10-06 14:30:08 +0200

    exo 4 Td 3 point a) debugging mode

--- TD3-Exo4RecursivityArray/main.c
+++ TD3-Exo4RecursivityArray/main.c
@@ -19,37 +19,59 @@ void afficherIterative(int tab[], int N){
 }
 
 void afficherRecursive(int tab[], int i){
-	if (i <= 0){
+	if (i < 0){
 			return ;
 	}
 	else{
 		afficherRecursive(tab, i -1);
-		printf("Recursive position %d: %d\n", i -1, tab[i-1]);
+		printf("Recursive position %d: %d\n", i , tab[i]);
+	}
+}
+
+/**
+ * @brief Printing Step by Step 
+ * @param tab: array to print
+ * @param i: current position to print
+ */
+void afficherRecursiveDebug(int tab[], int i){
+	printf("[Debug]Current value of i: %d\n", i);
+	if (i < 0){
+			printf("[Debug]Inside Stop condition: (negative position does not exist)\n");
+			return ;
+	}
+	else{
+		printf("[Debug]Before calling afficherRecursiveDebug(tab, %d)\n", i-1);
+		afficherRecursiveDebug(tab, i -1);
+		printf("Recursive position %d: %d\n", i , tab[i]);
 	}
 }
 
 void saisieRecursive(int tab[], int i){
-	if (i <= 0){
+	if (i < 0){
 			return ;
 	}
 	else{
 		saisieRecursive(tab, i -1);
-		printf("Enter value for position %d\n", i -1);
-		scanf("%d", &tab[i-1]);
+		printf("Enter value for position %d\n", i );
+		scanf("%d", &tab[i]);
 	}
 }
 int main(int argc, char **argv)
 {
 	printf("TD3\n");
-	int N=5;
-	int tab[N];
+	int sizeArray=5;
+	int tab[sizeArray];
 	//saisieIterative(tab, N);
 	//printf("Now we print the Array Iterative:\n");
 	//afficherIterative(tab, N);
+	
 	printf("Enter values for the array recursive: \n");
-	saisieRecursive(tab, N);
+	
+	int indexLastPosition = sizeArray -1;
+	saisieRecursive(tab, indexLastPosition);
 	printf("Print the array recursive: \n");
-	afficherRecursive(tab, N);
+	//afficherRecursive(tab, indexLastPosition);
+	afficherRecursiveDebug(tab, indexLastPosition);
 	
 	return 0;
 }
