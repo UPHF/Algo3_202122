827d5b354586bcd0b65f087aa58e21721e037a54
MohamedAmineNadim <81222687+MohamedAmineNadim@users.noreply.github.com>
2021-10-12 18:04:32 +0200

    Create main.c

---
+++ students_exercises_TDs/Mohamed_Amine_NADIM/TD3_Ex1/main.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+//Q1
+
+void longueur(char str[]){
+    int lng = 0, i = 0;
+    while (str[i] != '\0')
+    {
+        str[i] = tolower(str[i]);
+        lng += 1;
+        i++;
+    }
+    printf("La longueur de la chaine de caracteres '%s' est : %d \n", str, lng);
+}
+
+
+
+void afficheInverse(char str[]){
+    int l = 0, i = 0;
+
+    for (int i=0 ; str[i] != '\0' ; i++){
+        l++;
+    }
+
+    char inv[l];
+
+    while(str[i]!='\0')
+    {
+        inv[l] = tolower(str[i]);
+        i++;
+        l--;
+    }
+    printf("L'inverse de %s est :  %s\n", str, inv);
+}
+
+
+void sontEgales(char str1[], char str2[])
+{   int ln1 = 0, ln2 = 0, s = 0;
+    for (int i=0 ; str1[i] != '\0' ; i++){
+        ln1++;
+    }
+        for (int i=0 ; str2[i] != '\0' ; i++){
+        ln2++;
+    }
+    if (ln1 != ln2)
+    {
+        printf("Les chaines de caractere '%s' et '%s' sont differentes.\n", str1, str2);
+    }
+    else
+    {
+        while(str1[s]==str2[s])
+        {
+            s++;
+        }
+        if(s == ln1)
+        {
+                printf("Les chaines de caractere '%s' et '%s' sont egales.\n", str1, str2);
+        }
+        else
+        {
+                printf("Les chaines de caractere '%s' et '%s' sont differentes.\n", str1, str2);
+        }
+    }
+}
+
+
+
+int main()
+{
+    printf("TD3_Ex1\n");
+    char a[] = "chaine de test";
+    char b[] = "chaine de test";
+    char c[] = "autre chaine";
+    char b2[] = "chaine_de_test";
+    //Q1
+    longueur(a);
+
+    //Q2
+    afficheInverse(a);
+
+    //Q3
+    sontEgales(a,b);
+    sontEgales(a,c);
+    sontEgales(a,b2);
+
+    return 0;
+}
