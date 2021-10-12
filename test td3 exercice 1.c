00118bc7202da4faff9a7bbc8cc12c98e11f18b9
jeremy arciszewski <92380394+jerem-arcis@users.noreply.github.com>
2021-10-12 15:42:46 +0200

    Update test td3 exercice 1.c

--- test td3 exercice 1.c
+++ test td3 exercice 1.c
@@ -8,6 +8,30 @@ void longueur() {
    printf("la longueur du mot est de :%d\n",longueur);
    return 0;
 }
+///2
+void affiche_inverse()
+{
+    char chaine[20], inverse[20];
+    int t, i, j;
+
+    printf(" Entrez une chaine de caractere :  ");
+    gets(chaine);
+
+    j = 0;
+    t = strlen(chaine);
+
+    inverse[t] = '\0';
+    for (i = t - 1; i >= 0; i--)
+    {
+      inverse[j++] = chaine[i];
+    }
+    inverse[i] = '\0';
+
+    printf(" Chaine de caractère apres inversion = %s\n", inverse);
+
+    return 0;
+}
 main(){
 longueur();
+affiche_inverse();
 }
