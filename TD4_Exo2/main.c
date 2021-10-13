3826c8616c8bc94de8a1f69068d7fe8da1f68a56
martinezmatias <matias.sebastian.martinez@gmail.com>
2021-10-13 15:50:56 +0200

    bugfixing on permutation

--- TD4_Exo2/main.c
+++ TD4_Exo2/main.c
@@ -15,30 +15,34 @@ int retrieveIndexMinimale(Tab t, int i, int j){
 	int minIndex = i;
 	for (int k = i; k<=j;k++){
 			//printf("\n k %d ", k);
-		if (t[i]< minValue){
-				minValue = t[i];
-				minIndex = i;
+		if (t[k]< minValue){
+				minValue = t[k];
+				minIndex = k;
 			}
 	}
 	return minIndex;
 }
 
-int f1(Tab t){
+int sortPermutation(Tab t){
 	printf("In the beginning\n");
+	int nrPermute = 0;
 	for(int i = 0; i< N-1; i++){
 		printf("Analyzing i %d", i);
 		int m = retrieveIndexMinimale(t, i, N-1);
 		if (i != m){
-			permute(&t[i], &t[m]);/// permute(t+i,t+m);
+			permute(&t[i], &t[m]);
+			/// permute(t+i,t+m);
+			nrPermute++;
 			}
 		}
+		printf("\nNr permutations %d", nrPermute);
 }
 
 int main(int argc, char **argv)
 {
 	printf("TD4 exo 2\n");
-	Tab myTab = {3,44,8,70,21, 6, 40, 93, 28, 102};
-	f1(myTab);
+	Tab myTab = {3,17,9,15,2,7,10,9,8,12};//{3,44,8,70,21, 6, 40, 93, 28, 102};
+	sortPermutation(myTab);
 	printf("\nEnd");
 	for (int i = 0; i< N-1; i++){
 		printf("\n at %d: %d ", i, myTab[i]);
