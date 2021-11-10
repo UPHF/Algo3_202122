bfefaa99bd7864a1aab79edd989ab29096c11376
TanjiroFR <donovanpereira84@gmail.com>
2021-11-10 15:50:15 +0100

    TP2-QUESTION6-DONOVAN-PEREIRA.c

--- TD1-1/TP2-DONOVAN-PEREIRA.c
+++ TD1-1/TP2-DONOVAN-PEREIRA.c
@@ -31,7 +31,7 @@ void affichage(Tab t, int SizeTab){
     }
     else {
         affichage(t,SizeTab);
-        printf("Element at pos %d : %d\n", SizeTab, t[SizeTab]);
+        printf("Element a la position  %d = %d\n", SizeTab, t[SizeTab]);
     }
 }
 
@@ -96,18 +96,50 @@ if (i == n) return -1;
      recherche(t,n,i+1, rech);
 }
 
-int main(){
+/*Question 6*/
+
+void dichotomie (Tab t, int v){
+
+    //trie du tableau 
+        int i,minimun,j,tmp;
+        for(i=0;i<v-1;i++){
+                minimun=i;
+                for(j=i+1;j<v;j++)
+                    if(t[j]<t[minimun])
+                        minimun=j;
+                if(minimun!=i){
+                        tmp=t[i];
+                        t[i]=t[minimun];
+                        t[minimun]=tmp;
+                }
+        }
+        printf("Tableau trié : ");
+        for ( int i = 0; i < v; i++){
+        printf("%d/",t[i]);
+    }//fin du tri
+
+
+}
+
+int main(int argc, char *argv[]){
 
     printf("Algo-3 TP2-Donovan-Pereira.c\n");
 
     Tab t1 = {4,2,8,0,7,3,1,5,9,6};
     printf ("La somme des entiers du tableau t1 = %d\n",somme(t1,N,0));
-    affichage(t1, 10);
-    /*afficher(t1, 10); version deux*/
-    calcul(t1,N);
-    superieur(t1,N,0,2);
-    affichagedecroissants(t1,2);
-    printf("l'indice de recherche est %d",recherche(t1,N,0,2));
+    printf("\nQuestion 1\n");
+    affichage(t1, 10);/*Question 1*/
+    /*afficher(t1, 10); version deux*//*Question 1*/
+    printf("\nQuestion 2\n");
+    calcul(t1,N);/*Question 2*/
+    printf("\nQuestion 3\n");
+    superieur(t1,N,0,2);/*Question 3*/
+    printf("\nQuestion 4\n");
+    affichagedecroissants(t1,2);/*Question 4*/
+    printf("\nQuestion 5\n");
+    printf("l'indice de recherche est %d\n",recherche(t1,N,0,2));/*Question 5*/
+    printf("\nQuestion 6\n");
+    dichotomie(t1, 10);
 
     return 0;
 }
