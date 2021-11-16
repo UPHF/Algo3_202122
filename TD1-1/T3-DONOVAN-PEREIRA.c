d221b016a15d4fdf8926c4f4cc13deb77c796b18
TanjiroFR <donovanpereira84@gmail.com>
2021-11-16 10:30:10 +0100

    TP3-EXO3.c

--- TD1-1/T3-DONOVAN-PEREIRA.c
+++ TD1-1/T3-DONOVAN-PEREIRA.c
@@ -28,14 +28,22 @@ void affichage(Tab t, int SizeTab){
     }
 }
 
+void recopie(Tab t1, Tab t2 , int n){
+    for ( int i = 0; i < n; i++){
+        t2[i]=t1[i];
+        }
+    printf("\n");
+}
+
 int main (int argc, char *argv[]){
 
     printf("Algo-3 TP3-Donovan-Pereira.c\n");
 
-    Tab t1;
+    Tab t1, t2;
     
     remplirTableau(t1,N);
     affichage(t1, N);
+    recopie(t1, t2, N);
 
     return 0;
 }
